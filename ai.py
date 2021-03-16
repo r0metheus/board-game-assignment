78741455d30bdbff41d03df8c5674ad3030ff08a
Francesco Romeo <francesco_romeo@me.com>
2021-03-16 09:13:40 +0100

    created branch

--- ai.py
+++ ai.py
@@ -1,13 +1,37 @@
 import random
-from heuristics import heuristics
+from GameRules import valid_moves
+from heuristics import heuristic as h
+from Players import PLAYERA, AI
 
 class Agent:
-    def __init__(self):
-        self.name = self.spawn()
+    depth = 5   # review
     
-    def spawn(self):
-        return "Agent Smith " + str(random.randint(100, 200))
+    def __init__(self, board):
+        self.name = "Agent Smith " + str(random.randint(100, 200))
+        self.root = Node(board, PLAYERA)
 
     def turn(self, board):
-        # print("I see this board")
-        board.print_board()
+        print("from the main")
+        
+    def tree_build(self):
+        print("tree building")
+
+class Node:
+    def __init__(self, board, player):
+        self.state = board
+        self.value = self.evaluation()
+        self.player = player
+        self.children = []
+
+    def evaluation(self):
+        return h(self.state.get_board())
+
+    def get_board(self):
+        return self.state
+
+    def add_children(self, node):
+        self.children.append(node)
+    
+    def is_leaf(self):
+        return len(self.children) == 0
+
--- main.py
+++ main.py
@@ -36,8 +36,6 @@ coll_rects = []
 turn = PLAYERA
 agent = Agent()
 
-#print(agent.name)
-
 def create_coll(board):
     for row in range(len(board)):
         for col in range(len(board[0])):
@@ -94,6 +92,7 @@ ai_pos = [(0,6), (1,5), (1,6), (2,5), (2,6), (2,7), (3,4), (3,5), (3,6), (3,7)]
 #game loop
 while not done:
     if turn == AI:
+        agent.turn(board.get_board())
         random_idx = random.randint(0, 9)
         available_moves = valid_moves(board.get_board(), ai_pos[random_idx], AI)
         while len(available_moves) == 0:
