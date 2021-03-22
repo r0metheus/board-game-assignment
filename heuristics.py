28f1af67a8876dcaf0aa48c9cc664c70b37eb942
ZecchinLeonardo <leonardoz@live.it>
2021-03-22 20:39:33 +0100

    added clustering factor

--- heuristics.py
+++ heuristics.py
@@ -13,4 +13,9 @@ def heuristic(board, player):
   for index, value in np.ndenumerate(board):
     if value == player:
       sum = sum + distance.euclidean(index, goals[player])
+      for jndex,value in np.ndenumerate(board):
+        x=board[index[0],index[1]]
+        y=board[jndex[0],jndex[1]]
+        if((x==BLUE_PLAYER or x==RED_PLAYER) and x==y):
+          sum = sum+0.1*distance.euclidean(index,jndex)
   return round(sum, 2)
